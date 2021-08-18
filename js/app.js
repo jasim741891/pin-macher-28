0c28f91a87557d1ede133df6241641766ea46a5d
jasim741891 <jasimgai74@gmail.com>
2021-08-18 14:01:25 +0600

    pin dekano ok

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin
+    }
+    else {
+        // console.log('3 digite got', pin)
+
+        return getPin()
+
+    }
+}
+
+
+
+function generatePin() {
+
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
