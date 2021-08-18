12b1ed2e1736e0f9d2d2b91d6cfc3ca674eb0656
jasim741891 <jasimgai74@gmail.com>
2021-08-18 15:08:12 +0600

    code running

--- js/app.js
+++ js/app.js
@@ -19,3 +19,34 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+document.getElementById('key-paid').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const priviousCals = calcInput.value;
+        const newCals = priviousCals + number;
+        calcInput.value = newCals;
+    }
+
+})
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+
+        successMessage.style.display = 'block'
+        failError.style.display = 'none'
+    }
+    else {
+
+        failError.style.display = 'block'
+        successMessage.style.display = 'none'
+    }
+}
